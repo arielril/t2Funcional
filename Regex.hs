d9f49dd9de8d373d7bdce093af03e8a7621f6ab5
Ariel <ariel@whoami.local>
2016-10-25 09:06:23 -0200

    Files

--- Regex.hs
+++ Regex.hs
@@ -1,16 +1,1 @@
 module RegularExp where
-
-{-
-  Analisador de REGEX em Haskell
-
-Operadores:
-
-  String vazia           == eps
-  Literal                == char
-  Escolha (ou)           == |+|
-  Concatenacao           == |.|
-  Iteracao (Kleene star) == star
-
-
-
--}
